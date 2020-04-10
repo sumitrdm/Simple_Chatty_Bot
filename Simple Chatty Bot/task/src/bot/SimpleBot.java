419f4163906730e01982ddd80e4d09edaa088e7c
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2020-04-10 23:19:55 +0530

    Initial commit

---
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,14 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+    String x = "Aid";
+    int y =2018;
+
+    System.out.println("Hello! My name is"+ x);
+    System.out.println("I was created in" +y+".");
+
+
+
+    }
+}
